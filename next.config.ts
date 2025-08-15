3c1aef2479ee21f048a1f201e350e5da545ba2d9
Dipesh Chaulagain <dipesh@ngit.com.np>
2025-08-15 17:54:14 +0545

    feat: update Next.js configuration and add gh-pages support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // enables next export
+  images: {
+    unoptimized: true, // needed for GitHub Pages image compatibility
+  },
+  basePath: "/tx-body", // important if your repo is not root
+  assetPrefix: "/tx-body/",
 };
 
 export default nextConfig;
